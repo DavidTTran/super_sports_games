ce1db13f4845747b9c9033a31e6d730e21c12551
David Tran <davidttemail@gmail.com>
2020-01-30 13:21:45 -0700

    Calculate variance

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,13 @@
 ages = [24, 30, 18, 20, 41]
 
-# Your code here for calculating the standard deviation
+variance = 0 # variance equals each value squares, summed, then divided by the number of values
+
+ages.each do |age|
+  variance += (age ** 2)
+  variance / ages.count()
+end
+
+puts variance
+stand_deviation = 0
 
 # When you find the standard deviation, print it out
