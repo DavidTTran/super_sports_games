3ab7f8dd8e186f04335f06685fcb7e228a299c6d
David Tran <davidttemail@gmail.com>
2020-01-30 14:35:26 -0700

    Rewrite with setup

--- test/event_test.rb
+++ test/event_test.rb
@@ -3,39 +3,36 @@ require 'minitest/pride'
 require './lib/event'
 
 class EventTest < Minitest::Test
+  def setup
+    @event = Event.new("Curling", [24, 30, 18, 20, 41])
+  end
+
 
   def test_if_it_exists
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_instance_of Event, event
+    assert_instance_of Event, @event
   end
 
   def test_event_name
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_equal "Curling", event.name
+    assert_equal "Curling", @event.name
   end
 
   def test_event_ages
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_equal [24, 30, 18, 20, 41], event.ages
+    assert_equal [24, 30, 18, 20, 41], @event.ages
   end
 
   def test_max_age
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_equal 41, event.max_age
+    assert_equal 41, @event.max_age
   end
 
   def test_min_age
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_equal 18, event.min_age
+    assert_equal 18, @event.min_age
   end
 
   def test_average_age
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_equal 26.6, event.average_age
+    assert_equal 26.6, @event.average_age
   end
 
   def test_standard_deviation_age
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_equal 8.28, event.standard_deviation_age
+    assert_equal 8.28, @event.standard_deviation_age
   end
 end
