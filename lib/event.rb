476e7f677b08aa287be39a4c29344f73bf3e349f
David Tran <davidttemail@gmail.com>
2020-01-30 13:37:10 -0700

    Add max/min age methods and tests

---
+++ lib/event.rb
@@ -0,0 +1,16 @@
+class Event
+  attr_reader :name, :ages
+
+  def initialize(name, ages)
+    @name = name
+    @ages = ages
+  end
+
+  def max_age
+    @ages.max
+  end
+
+  def min_age
+    @ages.min
+  end
+end
