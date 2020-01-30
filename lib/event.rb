7126763ef5820fe42e17c388bf9d08ebab143ee6
David Tran <davidttemail@gmail.com>
2020-01-30 14:30:41 -0700

    Add standard deviation method and test

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,33 @@ class Event
     end
     return total_age / @ages.count.to_f
   end
+
+  def standard_deviation_age
+    total_age = 0
+    average_age = 0
+    variance_age_array = []
+    variance_age_difference = []
+    variance_age_sum = 0
+
+    @ages.each do |age|
+      total_age += age
+      average_age = (total_age / @ages.count.to_f)
+    end
+
+    @ages.each do |age|
+      variance_age_array << age - average_age
+    end
+
+    variance_age_array.each do |age|
+      variance_age_difference << age ** 2
+    end
+
+    variance_age_difference.each do |age|
+      variance_age_sum += age
+    end
+
+    variance_age = Math.sqrt(variance_age_sum / @ages.count)
+
+    return variance_age.round(2)
+  end
 end
